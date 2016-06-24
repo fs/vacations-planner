3e34b20277c4553d57def4e7a896abd579c7fca5
Daniil <daniil.sunyaev@flatstack.com>
2016-06-24 14:09:29 +0300

    rename schedule to vacations requests container

--- app/models/vacation_requests_container.rb
+++ app/models/vacation_requests_container.rb
@@ -1,4 +1,4 @@
-class Schedule
+class VacationRequestsContainer
   MIN_WEEKS_BETWEEN_VACATIONS = 8
   MIN_WEEKS_DURATION_FOR_LONGEST_VACATION = 2
   WEEKS_OF_VACATION_PER_YEAR = 4
@@ -6,32 +6,32 @@ class Schedule
 
   include ActiveModel::Validations
 
-  attr_accessor :schedule
+  attr_accessor :vacation_requests
 
   validate :elements_are_vacation_requests, :user_vacations_are_distanced,
     :at_least_one_vacation_is_long, :every_user_has_a_month_of_vacations
 
-  delegate :[], :[]=, :<<, to: :schedule
+  delegate :[], :[]=, :<<, to: :vacation_requests
 
   def initialize
-    @schedule = Array.new(WEEKS_IN_YEAR)
+    @vacation_requests = []
   end
 
   private
 
   def elements_are_vacation_requests
-    schedule.compact.each do |el|
-      errors.add(:schedule, "Contains non-request elements") unless el.is_a? VacationRequest
+    @vacation_requests.compact.each do |el|
+      errors.add(:@vacation_requests, "Contains non-request elements") unless el.is_a? VacationRequest
     end
   end
 
   def user_vacations_are_distanced
-    schedule.compact.group_by(&:user_id).each do |user_id, vacation_requests|
-      vacation_requests.combination(2).each do |vacation_pair|
+    @vacation_requests.compact.group_by(&:user_id).each do |user_id, user_vacation_requests|
+      user_vacation_requests.combination(2).each do |vacation_pair|
         next if (vacation_pair[0].ends_at_week - vacation_pair[1].starts_at_week).abs > MIN_WEEKS_BETWEEN_VACATIONS ||
                 (vacation_pair[1].ends_at_week - vacation_pair[0].starts_at_week).abs > MIN_WEEKS_BETWEEN_VACATIONS
         errors.add(
-          :schedule,
+          :vacation_requests,
           "Vacations with ids #{vacation_pair.map(&:id)} of user #{User.find(user_id).full_name} are planned to close"
         )
       end
@@ -39,17 +39,17 @@ class Schedule
   end
 
   def at_least_one_vacation_is_long
-    schedule.compact.group_by(&:user_id).each do |user_id, vacation_requests|
-      next if vacation_requests.max_by(&:length_in_weeks).length_in_weeks >= MIN_WEEKS_DURATION_FOR_LONGEST_VACATION
-      errors.add(:schedule, "User #{User.find(user_id).full_name} has many small vacations (< 14 days)")
+    @vacation_requests.compact.group_by(&:user_id).each do |user_id, user_vacation_requests|
+      next if user_vacation_requests.max_by(&:length_in_weeks).length_in_weeks >= MIN_WEEKS_DURATION_FOR_LONGEST_VACATION
+      errors.add(:vacation_requests, "User #{User.find(user_id).full_name} has many small vacations (< 14 days)")
     end
   end
 
   def every_user_has_a_month_of_vacations
-    schedule.compact.group_by(&:user_id).each do |user_id, vacation_requests|
-      next if vacation_requests.sum(&:length_in_weeks) == WEEKS_OF_VACATION_PER_YEAR
+    @vacation_requests.compact.group_by(&:user_id).each do |user_id, user_vacation_requests|
+      next if user_vacation_requests.sum(&:length_in_weeks) == WEEKS_OF_VACATION_PER_YEAR
       errors.add(
-        :schedule,
+        :vacation_requests,
         "User #{User.find(user_id).full_name} have planned vacation other than for a 28 days in a year"
       )
     end
--- spec/models/vacation_requests_container_spec.rb
+++ spec/models/vacation_requests_container_spec.rb
@@ -1,14 +1,14 @@
 require "rails_helper"
 
-describe Schedule do
+describe VacationRequestsContainer do
   describe "validations" do
     let(:user) { create :user }
 
     subject do
-      schedule = described_class.new
-      schedule[0] = vacation_request_1
-      schedule[20] = vacation_request_2
-      schedule
+      vacation_requests = described_class.new
+      vacation_requests[0] = vacation_request_1
+      vacation_requests[20] = vacation_request_2
+      vacation_requests
     end
 
     context "with vacations planeed too close" do
@@ -25,12 +25,12 @@ describe Schedule do
       let!(:vacation_request_4) { create :vacation_request, user: user, starts_at_week: 10, ends_at_week: 11 }
 
       subject do
-        schedule = described_class.new
-        schedule[0] = vacation_request_1
-        schedule[10] = vacation_request_2
-        schedule[20] = vacation_request_3
-        schedule[30] = vacation_request_3
-        schedule
+        vacation_requests = described_class.new
+        vacation_requests[0] = vacation_request_1
+        vacation_requests[10] = vacation_request_2
+        vacation_requests[20] = vacation_request_3
+        vacation_requests[30] = vacation_request_3
+        vacation_requests
       end
 
       it { is_expected.not_to be_valid }
